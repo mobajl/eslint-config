13ad549cf378c0b54480651b268395ea089e742e
Johan Kanefur <johan@kanefur.se>
2022-05-05 08:31:23 +0200

    refactor(config): Allow use in JS as well
    We want linting/fixing of the unmigrated JS code.
    
    Using "overrides" to make TS-specific rules.
    
    Inspiration:
    https://stackoverflow.com/questions/57597142/how-to-run-typescript-eslint-on-ts-files-and-eslint-on-js-files-in-the-same-pr
    
    parserOptions.sourceType = "module" and env.es2021 = true is necessary
    in order to use import keyword in .js files.

--- index.js
+++ index.js
@@ -2,27 +2,46 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
 	root: true,
-	parser: '@typescript-eslint/parser',
 	plugins: [
-		'@typescript-eslint',
 		'import'
 	],
+	env: {
+		es2021: true
+	},
+	parserOptions: {
+		sourceType: "module"
+	},
 	extends: [
 		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'prettier/@typescript-eslint',
 		'plugin:prettier/recommended'
 	],
-	rules: {
-		'import/order': [
-			'warn',
-			{
-				groups: [['builtin', 'external'], 'internal', ['parent', 'index', 'sibling']],
-				'newlines-between': 'always',
-				alphabetize: {
-					order: 'asc',
-				},
+	overrides: [
+		{
+			files: ['**/*.ts', '**/*.tsx'],
+			plugins: [
+				'import',
+				'@typescript-eslint',
+			],
+			extends: [
+				'eslint:recommended',
+				'plugin:@typescript-eslint/recommended',
+				'prettier/@typescript-eslint',
+				'plugin:prettier/recommended'
+			],
+			rules: {
+				'import/order': [
+					'warn',
+					{
+						groups: [['builtin', 'external'], 'internal', ['parent', 'index', 'sibling']],
+						'newlines-between': 'always',
+						alphabetize: {
+							order: 'asc',
+						},
+					},
+				],
 			},
-		],
-	}
+			parser: '@typescript-eslint/parser',
+		},
+	],
+
 }
