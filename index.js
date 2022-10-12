db6547097ad5cf06dd70b58ab59335036579f11d
Johan Kanefur <johan@kanefur.se>
2022-10-12 17:24:41 +0200

    Package: Upgrade eslint 7 -> 8 + cleanup

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-require("@rushstack/eslint-patch/modern-module-resolution");
-
 module.exports = {
 	root: true,
 	plugins: [
-		'import'
+		'@typescript-eslint',
+		'import',
+		'prettier'
 	],
 	env: {
 		es2021: true
@@ -13,35 +13,20 @@ module.exports = {
 	},
 	extends: [
 		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
 		'plugin:prettier/recommended'
 	],
-	overrides: [
-		{
-			files: ['**/*.ts', '**/*.tsx'],
-			plugins: [
-				'import',
-				'@typescript-eslint',
-			],
-			extends: [
-				'eslint:recommended',
-				'plugin:@typescript-eslint/recommended',
-				'prettier/@typescript-eslint',
-				'plugin:prettier/recommended'
-			],
-			rules: {
-				'import/order': [
-					'warn',
-					{
-						groups: [['builtin', 'external'], 'internal', ['parent', 'index', 'sibling']],
-						'newlines-between': 'always',
-						alphabetize: {
-							order: 'asc',
-						},
-					},
-				],
+	rules: {
+		'import/order': [
+			'warn',
+			{
+				groups: [['builtin', 'external'], 'internal', ['parent', 'index', 'sibling']],
+				'newlines-between': 'always',
+				alphabetize: {
+					order: 'asc',
+				},
 			},
-			parser: '@typescript-eslint/parser',
-		},
-	],
-
+		],
+	},
+	parser: '@typescript-eslint/parser',
 }
